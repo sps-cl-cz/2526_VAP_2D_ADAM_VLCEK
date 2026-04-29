9ec358b99adad0d543199dcb0ad79c50920e0cc3
Adam Vlček <Adam.Vlcek@student.sps-cl.cz>
2026-04-29 12:37:02 +0200

    Změny

--- NODE.JS/Express/app.js
+++ NODE.JS/Express/app.js
@@ -1,45 +1,80 @@
 const path = require('path');
 const express = require('express');
-
+const { error } = require('console');
+ 
 const app = express();
-
+ 
 const users = [];
-
+ 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public', 'views'));
-
+ 
 app.use(express.static(path.join(__dirname, "public")));
-
+ 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+ 
 app.get("/", (req, resp) => {
     resp.render("index")
 });  
-
+ 
 app.get("/register", (req, resp) => {
-    resp.sendFile(path.join(__dirname, "public", "views", "register.html"));
+    resp.render("register")
 });
-
+ 
 app.post("/register", (req, resp) => {
     const user = req.body;
-    users.push(user);
-    resp.redirect("/login");
+    const savedUser = users.find((v) => v.username === user.username);
+    if(savedUser){
+        resp.render("register", {error: "User already exists"});
+    }else {
+        users.push(user);
+        resp.redirect("/login");    
+    }
+   
 });
-
+ 
 app.get("/login", (req, resp) => {
-    resp.sendFile(path.join(__dirname, "public", "views", "login.html"));
+    resp.render("login")
 });
-
+ 
 app.post("/login", (req, resp) => {
     const {username, password} = req.body;
     const user = users.find((v) => v.username === username);
-    if(user) {     
+    if(user) {    
         return resp.render("index", {user});;
     }
     resp.redirect("/login");
 });
-
+ 
 app.listen(3000, () => {
     console.log("http://localhost:3000")
+});
+
+
+
+app.get("/reset_password", (req, resp) => {
+    resp.render("reset_password");
+});
+
+app.post("/reset_password", (req, resp) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+
+    const user = users.find((v) => v.username === username);
+
+    if (!user) {
+        resp.render("reset_password", { error: "Uživatel nebyl nalezen." });
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        resp.render("reset_password", { error: "Zadaná hesla se neshodují." });
+        return;
+    }
+
+    user.password = password;
+
+    resp.render("login");
 });
